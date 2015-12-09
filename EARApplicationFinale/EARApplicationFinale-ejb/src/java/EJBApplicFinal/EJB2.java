e22620b1c6dac0a2070654ed2143df5ad87448ba
Jérôme Fink <jeromefink@hotmail.com>
2015-12-09 21:16:54 +0100

    login ok (enfin)

--- EARApplicationFinale/EARApplicationFinale-ejb/src/java/EJBApplicFinal/EJB2.java
+++ EARApplicationFinale/EARApplicationFinale-ejb/src/java/EJBApplicFinal/EJB2.java
@@ -7,9 +7,11 @@ package EJBApplicFinal;
 
 import EntityClass.Client;
 import java.util.Arrays;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
