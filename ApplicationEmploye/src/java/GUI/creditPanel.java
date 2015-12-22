f2da8443052f7e073bf45e160e5b3b595dbec5cd
Jérôme Fink <jeromefink@hotmail.com>
2015-12-22 23:02:06 +0100

    début formulaire

--- ApplicationEmploye/src/java/GUI/creditPanel.java
+++ ApplicationEmploye/src/java/GUI/creditPanel.java
@@ -5,6 +5,8 @@
  */
 package GUI;
 
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author Jerome
@@ -27,30 +29,136 @@ public class creditPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        creditTitre = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+        attenteTitre = new javax.swing.JLabel();
+        deconnexionButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        informationSubTitle = new javax.swing.JLabel();
+        nomLabel = new javax.swing.JLabel();
+        nomTextField = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        prenomTextField = new javax.swing.JTextField();
+        adresseLAbel = new javax.swing.JLabel();
+        adresseTextField = new javax.swing.JTextField();
+        informationSubTitle1 = new javax.swing.JLabel();
 
-        creditTitre.setText("Credit");
+        jScrollPane1.setViewportView(jList1);
+
+        attenteTitre.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        attenteTitre.setText("Demandes en attente");
+
+        deconnexionButton.setText("Déconnexion");
+        deconnexionButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deconnexionButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("Nouvelle demande : ");
+
+        informationSubTitle.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        informationSubTitle.setText("Informations demandeur : ");
+
+        nomLabel.setText("Nom : ");
+
+        jLabel2.setText("Prénom : ");
+
+        adresseLAbel.setText("Adresse : ");
+
+        informationSubTitle1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        informationSubTitle1.setText("Informations sur le crédit : ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(284, 284, 284)
-                .addComponent(creditTitre)
-                .addContainerGap(342, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(deconnexionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(24, 24, 24))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(informationSubTitle)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(nomLabel)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(nomTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGap(37, 37, 37)
+                                    .addComponent(jLabel2)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(prenomTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(adresseLAbel)
+                                .addComponent(adresseTextField))
+                            .addComponent(informationSubTitle1))
+                        .addGap(89, 89, 89)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(attenteTitre)
+                                .addGap(38, 38, 38))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap())))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addComponent(creditTitre)
-                .addContainerGap(426, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(deconnexionButton)
+                    .addComponent(jLabel1))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(7, 35, Short.MAX_VALUE)
+                        .addComponent(attenteTitre)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 348, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(informationSubTitle)
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(nomLabel)
+                            .addComponent(nomTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2)
+                            .addComponent(prenomTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(adresseLAbel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(adresseTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(26, 26, 26)
+                        .addComponent(informationSubTitle1)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void deconnexionButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deconnexionButtonActionPerformed
+        //Changement de fenêtre
+        frameEmploye fe = (frameEmploye)SwingUtilities.getWindowAncestor(this);
+        fe.changeCard("connexion");
+    }//GEN-LAST:event_deconnexionButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel creditTitre;
+    private javax.swing.JLabel adresseLAbel;
+    private javax.swing.JTextField adresseTextField;
+    private javax.swing.JLabel attenteTitre;
+    private javax.swing.JButton deconnexionButton;
+    private javax.swing.JLabel informationSubTitle;
+    private javax.swing.JLabel informationSubTitle1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JList jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel nomLabel;
+    private javax.swing.JTextField nomTextField;
+    private javax.swing.JTextField prenomTextField;
     // End of variables declaration//GEN-END:variables
 }
